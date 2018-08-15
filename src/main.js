f3f611bd90cc29dd9fc0bb74d3c3b355f6c82682
asadahimeka <smallstar9637@yahoo.co.jp>
2018-08-15 11:52:19 +0800

    upd

--- src/main.js
+++ src/main.js
@@ -7,16 +7,8 @@ import '@components/_globals'
 // Don't warn about using the dev version of Vue in development
 Vue.config.productionTip = process.env.NODE_ENV === 'production'
 
-const app = new Vue({
+new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
-
-// If running inside Cypress
-if (window.Cypress) {
-  // Attach the app to the window, which can be useful
-  // for manually setting state in Cypress commands
-  // such as `cy.logIn()`
-  window.__app__ = app
-}
